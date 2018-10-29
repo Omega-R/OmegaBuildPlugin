1669e05c2c65f897ae266f3f63777e5b910288d8
Nikita Ivanov <nikita.ivanov@omega-r.com>
2018-10-29 10:48:53 +0300

    stash commit

--- BuildPlugin/src/main/java/com/omega/buildplugin/HttpLegacyFixTask.kt
+++ BuildPlugin/src/main/java/com/omega/buildplugin/HttpLegacyFixTask.kt
@@ -3,7 +3,12 @@ package com.omega.buildplugin
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.w3c.dom.Document
 import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
 
 open class HttpLegacyFixTask : DefaultTask() {
 
@@ -13,6 +18,19 @@ open class HttpLegacyFixTask : DefaultTask() {
     @Suppress("unused")
     @TaskAction
     fun fix() {
-        System.out.println("PUSHING HTTP LEGACY SUPPORT TO MANIFEST")
+        manifestFile?.let {
+            val manifestDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(it)
+            val root = manifestDoc.documentElement
+            val applicationNodeList = root.getElementsByTagName("")
+            if ()
+        }
+    }
+
+    private fun save(doc : Document) {
+        val transformer = TransformerFactory.newInstance().newTransformer()
+        val output = StreamResult(File("output.xml"))
+        val input = DOMSource(doc)
+
+        transformer.transform(input, output)
     }
 }
--- BuildPlugin/src/main/java/com/omega/buildplugin/OmegaBuildPlugin.kt
+++ BuildPlugin/src/main/java/com/omega/buildplugin/OmegaBuildPlugin.kt
@@ -15,33 +15,34 @@ class OmegaBuildPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val ext = project.extensions.create("networkBuild", NetworkBuildData::class.java)
 
-        val hasApp = project.plugins.hasPlugin(AppPlugin::class.java)
-        val hasLib = project.plugins.hasPlugin(LibraryPlugin::class.java)
-
-        if (!hasApp && !hasLib) {
-            throw IllegalStateException("'com.android.application' or 'com.android.library' plugin required.")
-        }
-
-        val variants: DomainObjectSet<out BaseVariant> = if (hasApp) {
-            project.extensions.findByType(AppExtension::class.java)!!.applicationVariants
-        } else {
-            project.extensions.findByType(LibraryExtension::class.java)!!.libraryVariants
-        }
-
-        variants.all { variant ->
-            variant.outputs.all { output ->
-                val manifestFile =
-                    File(output.processManifest.manifestOutputDirectory.absolutePath + "/AndroidManifest.xml")
-
-                val httpLegacyFixTask =
-                    project.tasks.create("preform${variant.name.capitalize()}${HttpLegacyFixTask::class.java.name}", HttpLegacyFixTask::class.java)
-                httpLegacyFixTask.manifestFile = manifestFile
-                httpLegacyFixTask.dependsOn.add("process${variant.name.capitalize()}Manifest")
-            }
-        }
-
         project.afterEvaluate {
             System.out.println("Passed domain = ${getDomain(ext.serverIp)}")
+            val hasApp = project.plugins.hasPlugin(AppPlugin::class.java)
+            val hasLib = project.plugins.hasPlugin(LibraryPlugin::class.java)
+
+            if (!hasApp && !hasLib) {
+                throw IllegalStateException("'com.android.application' or 'com.android.library' plugin required.")
+            }
+
+            val variants: DomainObjectSet<out BaseVariant> = if (hasApp) {
+                project.extensions.findByType(AppExtension::class.java)!!.applicationVariants
+            } else {
+                project.extensions.findByType(LibraryExtension::class.java)!!.libraryVariants
+            }
+
+            variants.all { variant ->
+                variant.outputs.all { output ->
+                    val manifestFile =
+                        File(output.processManifest.manifestOutputDirectory.absolutePath + "/AndroidManifest.xml")
+
+                    val httpLegacyFixTask = project.tasks.create(
+                        "preform${variant.name.capitalize()}${HttpLegacyFixTask::class.java.name}",
+                        HttpLegacyFixTask::class.java)
+                    httpLegacyFixTask.manifestFile = manifestFile
+                    httpLegacyFixTask.dependsOn.add("process${variant.name.capitalize()}Manifest")
+                    variant.register(httpLegacyFixTask)
+                }
+            }
         }
     }
 
